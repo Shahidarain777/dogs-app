a996182945026866954d8bccbcc435f29616a051
Shahid Ali <shahidarin777@gmail.com>
2023-11-25 15:43:48 +0500

    ok

--- dogs-app/app/(drawer)/(others)/ProfileSetting.tsx
+++ dogs-app/app/(drawer)/(others)/ProfileSetting.tsx
@@ -11,11 +11,29 @@ import {
 
 import PrimaryButton from "../../common/components/PrimaryButton";
 import { Link } from "expo-router";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 import colors from "../../res/colors";
+const countries = ["Egypt", "Canada", "Australia", "Ireland"];
 export default function Registration() {
   return (
     <View style={{ flex: 1 }}>
       <ScrollView contentContainerStyle={styles.container}>
+        <Link href={{ pathname: "AddOwner" }} asChild>
+          <Pressable>
+            <Text
+              style={{
+                textAlign: "right",
+                fontSize: 15,
+                fontFamily: "Unbounded",
+                marginTop: 50,
+                marginRight: 230,
+              }}
+            >
+              Back
+            </Text>
+          </Pressable>
+        </Link>
+
         <View style={{ flexDirection: "row", marginTop: 20 }}>
           <View
             style={{
@@ -33,21 +51,141 @@ export default function Registration() {
               source={require("../../res/images/pngs/black-foot.png")}
             />
           </View>
+
           <Text
             style={{
-              fontSize: 30,
+              fontSize: 22,
               color: colors["brown.900"],
               fontWeight: "bold",
               textAlign: "center",
+              marginTop: 6,
+              marginLeft: 10,
             }}
           >
             Profile Settings
           </Text>
         </View>
 
-        <Link href={{ pathname: "Login" }} asChild>
+        <View
+          style={{
+            borderBottomColor: colors["brown.900"],
+            borderBottomWidth: 2,
+            marginVertical: 20,
+            alignItems: "center",
+            marginTop: 0,
+          }}
+        >
+          <Text
+            style={{
+              width: 360,
+              fontSize: 14,
+              padding: 10,
+              color: colors["brown.900"],
+              textAlign: "center",
+            }}
+          />
+        </View>
+
+        <Text
+          style={{
+            fontSize: 12,
+            color: colors["brown.900"],
+            fontWeight: "600",
+            marginRight: 140,
+          }}
+        >
+          Change Name
+        </Text>
+
+        <View
+          style={{
+            borderBottomColor: colors["brown.900"],
+            borderBottomWidth: 1,
+            marginVertical: 20,
+            alignItems: "center",
+          }}
+        >
+          <TextInput
+            style={{
+              width: 230,
+              fontSize: 14,
+              padding: 10,
+              color: colors["brown.900"],
+              textAlign: "center",
+              marginTop: -30,
+            }}
+          />
+        </View>
+
+        <Text
+          style={{
+            fontSize: 12,
+            color: colors["brown.900"],
+            fontWeight: "600",
+            marginRight: 120,
+            marginTop: 40,
+          }}
+        >
+          Change Dog Breed
+        </Text>
+
+        <View
+          style={{
+            borderBottomColor: colors["brown.900"],
+            borderBottomWidth: 1,
+            marginVertical: 20,
+            alignItems: "center",
+          }}
+        >
+          <TextInput
+            style={{
+              width: 230,
+              fontSize: 14,
+              padding: 10,
+              color: colors["brown.900"],
+              textAlign: "center",
+              marginTop: -30,
+            }}
+          />
+        </View>
+
+        <Text
+          style={{
+            fontSize: 12,
+            color: colors["brown.900"],
+            fontWeight: "600",
+            marginRight: 120,
+            marginTop: 40,
+          }}
+        >
+          Change Dog Image
+        </Text>
+
+        <View
+          style={{
+            borderBottomColor: colors["brown.900"],
+            borderBottomWidth: 1,
+            marginVertical: 20,
+            alignItems: "center",
+          }}
+        >
+          <TextInput
+            style={{
+              width: 230,
+              fontSize: 14,
+              padding: 10,
+              color: colors["brown.900"],
+              textAlign: "center",
+              marginTop: -30,
+            }}
+          />
+        </View>
+
+        <View style={{ height: 50 }}></View>
+
+        <Link href={{ pathname: "ChangePassword" }} asChild>
           <Pressable>
-            <PrimaryButton title="Next" />
+            <PrimaryButton title="Submit" />
           </Pressable>
         </Link>
       </ScrollView>
